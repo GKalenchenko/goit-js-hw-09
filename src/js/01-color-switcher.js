513a593e8bcfa7ab09c09214ea46516eb0c8598f
Kalenchenko Gleb <gkalenchenko@gmail.com>
2022-12-04 11:31:03 +0300

    adds final edits to the first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+//---------------------------------------------------------------------
+let switchBodyColor = null;
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+startBtnRef.addEventListener('click', () => {
+  switchBodyColor = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnRef.setAttribute('disabled', 'disabled');
+});
+
+stopBtnRef.addEventListener('click', () => {
+  clearInterval(switchBodyColor);
+  startBtnRef.removeAttribute('disabled');
+});
