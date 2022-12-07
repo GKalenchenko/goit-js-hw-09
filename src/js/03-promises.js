8147a2be7c9de8a797091dc0b356948e27da8eae
Kalenchenko Gleb <107788336+GKalenchenko@users.noreply.github.com>
2022-12-07 22:48:31 +0300

    adds final changes to the third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget;
+
+  let amountVal = Number(amount.value);
+  let stepVal = Number(step.value);
+  let delayVal = Number(delay.value);
+
+  console.log(event.currentTarget);
+
+  for (let i = 1; i <= amountVal; i += 1) {
+    createPromise(i, delayVal)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayVal += stepVal;
   }
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
